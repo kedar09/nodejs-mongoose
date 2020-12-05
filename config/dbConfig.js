d56c3a79ae72513afc334847ab575587d3246e26
kedar09 <32976533+kedar09@users.noreply.github.com>
2020-12-06 00:25:45 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,16 +2,7 @@
 const url = 'mongodb://localhost:27017/';
 
 // Database Name
-const dbName = 'github';
+const dbName = 'TEST';
 
 const dbUrl = url+dbName;
 module.exports = dbUrl;
-
-// const mongoose = require("mongoose");
-// mongoose.connect("mongodb://localhost:27017/github", { useNewUrlParser: true });
-
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error:"));
-// db.once("open", function () {
-//   console.log("MongoDB Connected");
-// });
